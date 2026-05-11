fe5e8290c93c6b073f92515973dfcbd3398f201d
cleverunemployed <agonycavarly@gmail.com>
2026-05-11 15:42:37 +0400

    Created base server on axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello world!" }));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Server listener addr: {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
 }
